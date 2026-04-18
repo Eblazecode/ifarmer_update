0f24b2d018fd0e1a9d88bf30a1985e60dfbde1fb
Eblazecode <dynablaze@gmail.com>
2026-04-18 16:08:57 +0100

    upgrade for SEO

---
+++ src/lib/utils.ts
@@ -0,0 +1,19 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: string) {
+  return new Intl.DateTimeFormat("en-NG", {
+    dateStyle: "medium"
+  }).format(new Date(date));
+}
+
+export function slugifyText(value: string) {
+  return value
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/(^-|-$)+/g, "");
+}
